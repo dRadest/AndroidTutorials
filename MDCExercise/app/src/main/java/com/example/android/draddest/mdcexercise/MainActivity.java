4c1f05f3f84e2358c4fcf500eb10b73111d682c1
DRad <progdrad@gmail.com>
2017-06-26 16:49:54 +0200

    MDCExercise: empty navigation drawer

--- MDCExercise/app/src/main/java/com/example/android/draddest/mdcexercise/MainActivity.java
+++ MDCExercise/app/src/main/java/com/example/android/draddest/mdcexercise/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.android.draddest.mdcexercise;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // instance variable for the drawer layout
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create Navigation drawer and inflate layout
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        // Adding menu icon to Toolbar
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Set behavior of Navigation drawer
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    // This method will trigger on item Click of navigation menu
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // Set item in checked state
+                        menuItem.setChecked(true);
+                        // handle navigation
+                        int id = menuItem.getItemId();
+                        switch (id){
+                            case R.id.first_drawer_item:
+                                Toast.makeText(MainActivity.this, "Sit back and relax", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.second_drawer_item:
+                                Toast.makeText(MainActivity.this, "Hasta la victoria siempre", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.third_drawer_item:
+                                Toast.makeText(MainActivity.this, "IT rocks", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        // Closing drawer on item click
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
