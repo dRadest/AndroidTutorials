60265b38d5bb83c8092ab345ae82b1caf3f04f77
DRad <progdrad@gmail.com>
2017-06-28 08:55:26 +0200

    MDCExercise: add RecyclerView to main activity and an adapter to populate it

--- MDCExercise/app/src/main/java/com/example/android/draddest/mdcexercise/MainActivity.java
+++ MDCExercise/app/src/main/java/com/example/android/draddest/mdcexercise/MainActivity.java
@@ -1,23 +1,45 @@
 package com.example.android.draddest.mdcexercise;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.support.design.widget.NavigationView;
+import android.support.v4.graphics.ColorUtils;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.os.Build.VERSION_CODES.N;
 
 public class MainActivity extends AppCompatActivity {
 
     // instance variable for the drawer layout
     private DrawerLayout mDrawerLayout;
 
+    // number of items to display in our list
+    private static final int NUMBER_OF_ITEMS = 56;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initializing the recycler view and setting the adapter on it
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ItemAdapter adapter = new ItemAdapter(NUMBER_OF_ITEMS);
+        recyclerView.setAdapter(adapter);
 
         // Create Navigation drawer and inflate layout
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
@@ -65,5 +87,52 @@ public class MainActivity extends AppCompatActivity {
             mDrawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // simple view holder for the recycler view
+    public static class ItemViewHolder extends RecyclerView.ViewHolder{
+        private TextView itemIdView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            itemIdView = (TextView) itemView.findViewById(R.id.item_id);
+        }
+
+        public void bind(int indexPosition){
+            itemIdView.setText(String.valueOf(indexPosition));
+        }
+    }
+
+    // adapter to populate the views
+    public static class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder>{
+
+        private int mNumberOfItems;
+
+        public ItemAdapter(int numberOfItems){
+            mNumberOfItems = numberOfItems;
+        }
+
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            int layoutIdForListItem = R.layout.list_item;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
+
+            View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+            ItemViewHolder viewHolder = new ItemViewHolder(view);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ItemViewHolder holder, int position) {
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNumberOfItems;
+        }
     }
 }
