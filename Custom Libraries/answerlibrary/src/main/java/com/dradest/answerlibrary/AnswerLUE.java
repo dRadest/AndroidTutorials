00cbc8aec2e15e1f58093f35e14a021eaaf40492
DRad <progdrad@gmail.com>
2019-02-27 08:11:10 +0100

    Add custom Java library

--- Custom Libraries/answerlibrary/src/main/java/com/dradest/answerlibrary/AnswerLUE.java
+++ Custom Libraries/answerlibrary/src/main/java/com/dradest/answerlibrary/AnswerLUE.java
@@ -2,7 +2,7 @@ package com.dradest.answerlibrary;
 
 public class AnswerLUE {
     // method that returns the answer to life, the universe and everything
-    public int giveMeTheAnswer(){
-        return 42;
+    public String giveMeTheAnswer(){
+        return "42";
     }
 }
--- Custom Libraries/app/src/main/java/com/dradest/customlibraries/MainActivity.java
+++ Custom Libraries/app/src/main/java/com/dradest/customlibraries/MainActivity.java
@@ -2,12 +2,33 @@ package com.dradest.customlibraries;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dradest.answerlibrary.AnswerLUE;
 
 public class MainActivity extends AppCompatActivity {
+    Button mLUEButton;
+    TextView mAnswerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the answer
+        AnswerLUE answerHolder = new AnswerLUE();
+        final String theAnswer = answerHolder.giveMeTheAnswer();
+
+        mAnswerView = findViewById(R.id.ans_tv);
+        mLUEButton = findViewById(R.id.lue_btn);
+        mLUEButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAnswerView.setText(theAnswer);
+            }
+        });
     }
 }
