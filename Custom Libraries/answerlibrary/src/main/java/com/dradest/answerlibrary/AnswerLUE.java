cab61c375eff8b93df5137b8b030b6d945763feb
DRad <progdrad@gmail.com>
2019-02-27 07:29:58 +0100

    Add custom Java library

---
+++ Custom Libraries/answerlibrary/src/main/java/com/dradest/answerlibrary/AnswerLUE.java
@@ -0,0 +1,8 @@
+package com.dradest.answerlibrary;
+
+public class AnswerLUE {
+    // method that returns the answer to life, the universe and everything
+    public int giveMeTheAnswer(){
+        return 42;
+    }
+}
