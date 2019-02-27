5c94a72a1cc970b1d92415dc785784003f93f380
DRad <progdrad@gmail.com>
2019-02-27 11:27:09 +0100

    Add custom Android library

---
+++ Custom Libraries/aarapplication/src/main/java/com/dradest/aarapplication/AARActivity.java
@@ -0,0 +1,13 @@
+package com.dradest.aarapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aar_activity_main);
+    }
+}
--- Custom Libraries/app/src/main/java/com/dradest/customlibraries/MainActivity.java
+++ Custom Libraries/app/src/main/java/com/dradest/customlibraries/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dradest.customlibraries;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,10 +8,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.dradest.aarapplication.AARActivity;
 import com.dradest.answerlibrary.AnswerLUE;
+
 
 public class MainActivity extends AppCompatActivity {
     Button mLUEButton;
+    Button mAARButton;
     TextView mAnswerView;
 
     @Override
@@ -28,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mAnswerView.setText(theAnswer);
+            }
+        });
+        mAARButton = findViewById(R.id.aar_btn);
+        mAARButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AARActivity.class);
+                startActivity(intent);
             }
         });
     }
